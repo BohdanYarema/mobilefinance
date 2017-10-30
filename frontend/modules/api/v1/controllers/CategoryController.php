6a5156d2fef84d67ae6914f7e3b3b973b7fc0e0f
Bogdanek <bogdan@aurocraft.com>
2017-10-31 00:14:51 +0200

    revert category controller

--- frontend/modules/api/v1/controllers/CategoryController.php
+++ frontend/modules/api/v1/controllers/CategoryController.php
@@ -1,12 +1,12 @@
 <?php
 namespace frontend\modules\api\v1\controllers;
 
+use frontend\modules\api\v1\components\CorsCustom;
 use frontend\modules\api\v1\filters\HttpBearerAuth;
 use Yii;
 use yii\filters\AccessControl;
 use yii\filters\Cors;
 use yii\rest\ActiveController;
-use yii\web\Response;
 
 /**
  * Category controller
@@ -28,58 +28,38 @@ class CategoryController extends ActiveController
 
     public function behaviors()
     {
-//        $behaviors = parent::behaviors();
-//        unset($behaviors['authenticator']);
-//
-//        $behaviors['authenticator'] = [
-//            'class'     => HttpBearerAuth::className(),
-//            'only'      => ['index'],
-//            'except'    => ['options'],
-//        ];
-//        $behaviors['access'] = [
-//            'class' => AccessControl::className(),
-//            'only' => ['index'],
-//            'rules' => [
-//                [
-//                    'actions' => ['index'],
-//                    'allow' => true,
-//                    'roles' => ['@'],
-//                ],
-//                [
-//                    'allow' => true,
-//                    'actions' => ['options'],
-//                    'roles' => ['?'],
-//                ],
-//            ],
-//        ];
-//
-//        $behaviors[] = [
-//            'class' => \yii\filters\ContentNegotiator::className(),
-//            'only' => ['index'],
-//            'formats' => [
-//                'application/json' => \yii\web\Response::FORMAT_JSON,
-//            ],
-//        ];
+        $behaviors['authenticator'] = [
+            'class'     => HttpBearerAuth::className(),
+            'only'      => ['index'],
+            'except'    => ['options'],
+        ];
 
-        $behaviors = parent::behaviors();
-        unset($behaviors['authenticator']);
-        return [
-            'corsFilter' => [
-                'class' => \yii\filters\Cors::className(),
-                'cors' => [
-                    'Origin' => ['*'],
-                    'Access-Control-Request-Method' => ['GET', 'POST', 'OPTIONS'],
-                    'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
+        $behaviors['access'] = [
+            'class' => AccessControl::className(),
+            'only' => ['index'],
+            'rules' => [
+                [
+                    'actions' => ['index'],
+                    'allow' => true,
+                    'roles' => ['@'],
                 ],
-            ],
-            'authenticator' => [
-                'class' => HttpBearerAuth::className(),
-                'except' => ['options'],
+                [
+                    'allow' => true,
+                    'actions' => ['options'],
+                    'roles' => ['?'],
+                ],
             ],
         ];
 
-        //return $behaviors;
+        $behaviors[] = [
+            'class' => \yii\filters\ContentNegotiator::className(),
+            'only' => ['index'],
+            'formats' => [
+                'application/json' => \yii\web\Response::FORMAT_JSON,
+            ],
+        ];
+
+        return $behaviors;
     }
 
     /**
