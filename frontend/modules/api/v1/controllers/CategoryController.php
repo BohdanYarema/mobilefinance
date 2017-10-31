0d68b9cdce564d39cdaa0e9cd25fc2b57a5ba102
Yarema Bohdan <b.yarema@mobimill.com>
2017-10-31 14:07:56 +0200

    test except

--- frontend/modules/api/v1/controllers/CategoryController.php
+++ frontend/modules/api/v1/controllers/CategoryController.php
@@ -41,29 +41,24 @@ class CategoryController extends ActiveController
     public function behaviors()
     {
         $behaviors = parent::behaviors();
-
-        // remove authentication filter
-        $auth = $behaviors['authenticator'];
         unset($behaviors['authenticator']);
 
-        // add CORS filter
         $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::className(),
+            'class' => Cors::className(),
             'cors' =>  [
                 'Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
                 'Access-Control-Allow-Credentials' => null,
-                'Access-Control-Max-Age' => 0,
+                'Access-Control-Max-Age' => 86400,
                 'Access-Control-Expose-Headers' => []
             ]
         ];
 
-        // re-add authentication filter
-        $behaviors['authenticator'] = $auth;
-        $behaviors['authenticator']['class'] = HttpBearerAuth::className();
-        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
-        $behaviors['authenticator']['only'] = ['index'];
+        $behaviors['authenticator'] = [
+            'class' =>  HttpBearerAuth::className(),
+            'except' => ['options','index'],
+        ];
 
         return $behaviors;
     }
