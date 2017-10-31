ef155f0f277efcb1480ea17303b9fca8c27500cf
Yarema Bohdan <b.yarema@mobimill.com>
2017-10-31 12:53:44 +0200

    tes in index

--- frontend/modules/api/v1/controllers/CategoryController.php
+++ frontend/modules/api/v1/controllers/CategoryController.php
@@ -72,12 +72,6 @@ class CategoryController extends ActiveController
 
         $behaviors['corsFilter'] = [
             'class' => Cors::className(),
-            'cors' => [
-                'Origin' => ['*'],
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Credentials' => true,
-            ],
         ];
 
         $behaviors['authenticator'] = [
