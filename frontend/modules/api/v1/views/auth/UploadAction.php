6432afbcbfecb747154943338c4175469daf56f7
Bogdanek <bogdan@aurocraft.com>
2017-11-07 21:36:06 +0200

    test upload

--- frontend/modules/api/v1/views/auth/UploadAction.php
+++ frontend/modules/api/v1/views/auth/UploadAction.php
@@ -6,6 +6,7 @@ use backend\models\SystemLog;
 use Yii;
 use yii\rest\Action;
 use yii\web\HttpException;
+use yii\web\UploadedFile;
 
 class UploadAction extends Action
 {
@@ -14,16 +15,35 @@ class UploadAction extends Action
      */
     public function run()
     {
-        $model = new SystemLog();
-        $model->level = 4;
-        $model->log_time = time();
-        $model->prefix = "test";
-        $model->category = 'test';
-        $model->message = json_encode(['file' => $_FILES, 'post' => $_POST]);
+//        $model = new SystemLog();
+//        $model->level = 4;
+//        $model->log_time = time();
+//        $model->prefix = "test";
+//        $model->category = 'test';
+//        $model->message = json_encode(['file' => $_FILES]);
+//        $model->save();
+//
+//        if ($model->save()){
+//            return true;
+//        } else {
+//            return false;
+//        }
+        $data = $_FILES;
+        //$uploads = UploadedFile::getInstancesByName($_FILES['upfile']);
+        $uploads = UploadedFile::getInstanceByName("upfile");
 
+//        $_FILES = [
+//            'upfile' => [
+//                'name' => 'Знімок екрана 2017-11-02 о 23.31.06.png',
+//                'type' => 'image/png',
+//                'tmp_name' => '/private/var/tmp/php6KOugU',
+//                'error' => 0,
+//                'size' => 199634,
+//            ],
+//        ];
 
+        $uploads->saveAs(Yii::getAlias("@web/fileupload/").'test.jpg');
 
-        $model->save();
-        exit();
+        var_dump($uploads);
     }
 }
