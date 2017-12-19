063980c185bbae7ca2a62c6e86e11e8a4e319226
Bogdanek <bogdan@aurocraft.com>
2017-12-20 00:01:51 +0200

    rebuild file upload

--- frontend/modules/api/v1/views/auth/UploadAction.php
+++ frontend/modules/api/v1/views/auth/UploadAction.php
@@ -3,6 +3,7 @@
 namespace frontend\modules\api\v1\views\auth;
 
 use backend\models\SystemLog;
+use common\models\UserProfile;
 use Yii;
 use yii\rest\Action;
 use yii\web\HttpException;
@@ -23,8 +24,12 @@ class UploadAction extends Action
             $ext = $uploads->getExtension();
             $uploads->saveAs(Yii::getAlias('@webroot/fileupload').'/'.Yii::$app->user->id."_".time()."_user_logo_".$ext);
 
-            var_dump($uploads);
-            exit();
+            var_dump(Yii::getAlias('@webroot/fileupload'));
+
+            $model  = UserProfile::findOne(Yii::$app->user->id);
+            $model->avatar_base_url = 'http://storage.mobilefinance.local.dev/source';
+            $model->avatar_path     = '1/f1pVFmRXHzARx7FtW0Sfp4sjKqI-1aM0.jpg';
+            $model->save();
 
             $response = Yii::$app->getResponse();
             $response->setStatusCode(200);
