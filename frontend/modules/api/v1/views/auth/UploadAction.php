68a45b1024fbdafa7fb3c3174f25ad2d77b2a5f4
Bogdanek <bogdan@aurocraft.com>
2017-12-20 00:55:30 +0200

    add saving file

--- frontend/modules/api/v1/views/auth/UploadAction.php
+++ frontend/modules/api/v1/views/auth/UploadAction.php
@@ -27,7 +27,7 @@ class UploadAction extends Action
 
             $model  = UserProfile::find()->where(['user_id' => Yii::$app->user->id])->one();
             $model->avatar_base_url = Yii::getAlias('@storageUrl').'/source';
-            $model->avatar_path     = '1/'.$name;
+            $model->avatar_path     = '1/';
             $model->save();
 
             $response = Yii::$app->getResponse();
