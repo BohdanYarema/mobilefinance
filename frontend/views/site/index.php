64040745724d80974b47c037618481eb0cb5658c
Bogdanek <bogdan@aurocraft.com>
2017-12-25 23:07:24 +0200

    fix frontend

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,15 +9,3 @@
 ?>
 
 <h1>Hi on our site !)))</h1>
-
-<?php
-    var_dump(Yii::getAlias('@storageUrl'));
-$model  = \common\models\UserProfile::find()->where(['user_id' => Yii::$app->user->id])->one();
-var_dump($model);
-var_dump(Yii::$app->user->id);
-
-$model  = \frontend\models\UserProfile::find()->where(['user_id' => 1])->one();
-$model->avatar_base_url = Yii::getAlias('@storageUrl').'/source';
-$model->avatar_path     = '1/';
-$model->save();
-?>
