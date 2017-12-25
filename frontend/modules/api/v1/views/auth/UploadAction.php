0d3b37d237104a51f9adb1d12c7e3a1a416e58e9
bogdan <bohdanyarema1992@gmail.com>
2017-12-25 13:48:06 +0200

    test upload

--- frontend/modules/api/v1/views/auth/UploadAction.php
+++ frontend/modules/api/v1/views/auth/UploadAction.php
@@ -21,19 +21,20 @@ class UploadAction extends Action
         if ($uploads == null){
             throw new ServerErrorHttpException('Failed to create the object for unknown reason.');
         } else {
-            $ext = $uploads->getExtension();
-            $name = Yii::$app->user->id."_".time()."_user_logo_".$ext;
-            $uploads->saveAs(Yii::getAlias('@storage/web/source/1/').$name);
-
-            $model  = UserProfile::find()->where(['user_id' => 1])->one();
-            $model->avatar_base_url = Yii::getAlias('@storageUrl').'/source';
-            $model->avatar_path     = '1/'.$name;
-            $model->save();
-
-            echo $model->getAvatar();
-
-            $response = Yii::$app->getResponse();
-            $response->setStatusCode(200);
+            var_dump(json_encode($uploads));
+//            $ext = $uploads->getExtension();
+//            $name = Yii::$app->user->id."_".time()."_user_logo_".$ext;
+//            $uploads->saveAs(Yii::getAlias('@storage/web/source/1/').$name);
+//
+//            $model  = UserProfile::find()->where(['user_id' => 1])->one();
+//            $model->avatar_base_url = Yii::getAlias('@storageUrl').'/source';
+//            $model->avatar_path     = '1/'.$name;
+//            $model->save();
+//
+//            echo $model->getAvatar();
+//
+//            $response = Yii::$app->getResponse();
+//            $response->setStatusCode(200);
         }
     }
 }
