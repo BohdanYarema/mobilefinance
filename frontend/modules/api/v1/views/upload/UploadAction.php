dbdc6fa5c2c8d103b7db093d4e6e7128ffb240ee
bogdan <bohdanyarema1992@gmail.com>
2017-12-26 14:47:45 +0200

    test extension

--- frontend/modules/api/v1/views/upload/UploadAction.php
+++ frontend/modules/api/v1/views/upload/UploadAction.php
@@ -24,25 +24,20 @@ class UploadAction extends Action
             $id = 1;
             $ext = $uploads->getExtension();
 
-            var_dump($ext);
-            var_dump($uploads);
             $name = $id."_".time()."_user_logo.".$ext;
-            var_dump($name);
             $uploads->saveAs(Yii::getAlias('@storage/web/source/1/').$name);
-            var_dump($uploads);
-            exit();
-//
-//            $model = UserProfile::find()->where(['user_id' => $id])->one();
-//            $model->avatar_base_url = Yii::getAlias('@storageUrl').'/source';
-//            $model->avatar_path     = '1/'.$name;
-//            $model->save();
-//
-//            return [
-//                'image'   => $model->avatar_base_url.'/'.$model->avatar_path,
-//                'code'    => 1,
-//                "status"  => 200,
-//                "message" => "Upload successful.",
-//            ];
+
+            $model = UserProfile::find()->where(['user_id' => $id])->one();
+            $model->avatar_base_url = Yii::getAlias('@storageUrl').'/source';
+            $model->avatar_path     = '1/'.$name;
+            $model->save();
+
+            return [
+                'image'   => $model->avatar_base_url.'/'.$model->avatar_path,
+                'code'    => 1,
+                "status"  => 200,
+                "message" => "Upload successful.",
+            ];
         }
     }
 }
