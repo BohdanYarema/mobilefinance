65043007809be8370279943a7912aad0dbde96a4
bogdan <bohdanyarema1992@gmail.com>
2017-12-26 17:37:49 +0200

    add more info about user

--- frontend/modules/api/v1/views/auth/SignupAction.php
+++ frontend/modules/api/v1/views/auth/SignupAction.php
@@ -30,6 +30,11 @@ class SignupAction extends Action
                     'access_token'  => Yii::$app->user->identity->getAuthKey(),
                     'username'      => Yii::$app->user->identity->username,
                     'email'         => Yii::$app->user->identity->email,
+                    'avatar'        => Yii::$app->user->identity['userProfile']->getAvatar(),
+                    'firstname'     => Yii::$app->user->identity['userProfile']->firstname,
+                    'lastname'      => Yii::$app->user->identity['userProfile']->lastname,
+                    'middlename'    => Yii::$app->user->identity['userProfile']->middlename,
+                    'gender'        => Yii::$app->user->identity['userProfile']->gender,
                     'created_at'    => Yii::$app->user->identity->created_at,
                 ]);
             } else {
