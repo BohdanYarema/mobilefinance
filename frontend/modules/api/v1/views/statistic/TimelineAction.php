ba5040786cc226d5e266a84c3d9ee1cf3284f39e
bogdan <bohdanyarema1992@gmail.com>
2018-02-06 18:01:18 +0200

    fix parse data

--- frontend/modules/api/v1/views/statistic/TimelineAction.php
+++ frontend/modules/api/v1/views/statistic/TimelineAction.php
@@ -19,13 +19,13 @@ class TimelineAction extends Action
      * @return array
      * @var $id integer
      */
-    public function run($month, $year)
+    public function run($year, $month)
     {
         if ($this->checkAccess) {
             call_user_func($this->checkAccess, $this->id);
         }
 
-        echo json_encode($this->prepareDataProvider($month, $year));
+        echo json_encode($this->prepareDataProvider($year, $month));
     }
 
     /**
@@ -33,42 +33,91 @@ class TimelineAction extends Action
      * @return array
      * @var $id integer
      */
-    protected function prepareDataProvider($month, $year)
+    protected function prepareDataProvider($year, $month)
     {
-        $model = Accounting::find()
-            ->select(['dates'])
-            ->groupBy(['dates'])
-            ->all();
+        $result = Accounting::find()->all();
 
-        $result     = [];
-        $response   = [];
-        foreach ($model as $item) {
-            if (date('n',$item->dates) == $month && date('Y',$item->dates) == $year){
-                $data = Accounting::find()
-                    ->where(['dates' => $item])
-                    ->all();
-                $responses  = [];
-                if (!empty($data)){
-                    foreach ($data as $key => $value){
-                        $response['color']      = $value->category->color;
-                        $response['id']         = $value->id;
-                        $response['gps_x']      = $value->gps_x;
-                        $response['gps_y']      = $value->gps_y;
-                        $response['price']      = $value->price;
-                        $response['name']       = $value->name;
-                        $response['time']       = date('d-m-Y', $value->dates);
-                        $response['dates']      = $value->dates;
-                        $response['avatar']     = $value->category->thumbnail_base_url."/".$value->category->thumbnail_path;;
-                        $responses[] = $response;
-                    }
+        $data = [];
+        $final = [];
+        $response = [];
 
-                    $result[$item->dates] = [
-                        'dates' => date('l, F j', $item->dates),
-                        'data'  => $responses
-                    ];
+        foreach ($result as $item){
+            $years   = date("Y", $item->dates);
+            $data[$years] = [];
+            foreach ($result as $value){
+                if($years === date("Y", $value->dates)){
+                    $data[$years][] = $value;
                 }
             }
         }
-        return $result;
+
+        foreach ($data as $key => $value){
+            foreach ($value as $item) {
+                switch (date("n", $item->dates)){
+                    case 1:
+                        $response[$key][1][] = $item;
+                        break;
+                    case 2:
+                        $response[$key][2][] = $item;
+                        break;
+                    case 3:
+                        $response[$key][3][] = $item;
+                        break;
+                    case 4:
+                        $response[$key][4][] = $item;
+                        break;
+                    case 5:
+                        $response[$key][5][] = $item;
+                        break;
+                    case 6:
+                        $response[$key][6][] = $item;
+                        break;
+                    case 7:
+                        $response[$key][7][] = $item;
+                        break;
+                    case 8:
+                        $response[$key][8][] = $item;
+                        break;
+                    case 9:
+                        $response[$key][9][] = $item;
+                        break;
+                    case 10:
+                        $response[$key][10][] = $item;
+                        break;
+                    case 11:
+                        $response[$key][11][] = $item;
+                        break;
+                    case 12:
+                        $response[$key][12][] = $item;
+                        break;
+                }
+            }
+        }
+
+        foreach ($response as $key => $years){
+            foreach ($years as $months => $item) {
+                foreach ($item as $day) {
+                    $date = date('j', $day->dates);
+                    $final[$key][$months][date('l, F j', $day->dates)] = [];
+                    $finder = [];
+                    foreach ($item as $value) {
+                        if ($date == date('j', $value->dates)){
+                            $finder['color']      = $value->category->color;
+                            $finder['id']         = $value->id;
+                            $finder['gps_x']      = $value->gps_x;
+                            $finder['gps_y']      = $value->gps_y;
+                            $finder['price']      = $value->price;
+                            $finder['name']       = $value->name;
+                            $finder['time']       = date('d-m-Y', $value->dates);
+                            $finder['dates']      = $value->dates;
+                            $finder['avatar']     = $value->category->thumbnail_base_url."/".$value->category->thumbnail_path;
+                            $final[$key][$months][date('l, F j', $day->dates)][] = $finder;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $final[$year][$month];
     }
 }
